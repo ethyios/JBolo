80f8de6331d87b754f3b30f9c9cf4cbcda1b8c8f
ethyios <gugagom1998@gmail.com>
2023-05-19 21:10:24 -0300

    Continuação da implementação das classes

--- src/Bolo.java
+++ src/Bolo.java
@@ -2,4 +2,10 @@ public class Bolo {
     Tipo tipo;
     Sabor sabor;
     int tamanho;
+
+    public Bolo(Tipo tipo, Sabor sabor, int tamanho) {
+        this.tipo = tipo;
+        this.sabor = sabor;
+        this.tamanho = tamanho;
+    }
 }
--- src/Sabor.java
+++ src/Sabor.java
@@ -1,4 +1,9 @@
 public class Sabor {
     int id;
     String nome;
+    
+    public Sabor(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
 }
